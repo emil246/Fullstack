5bdf246dbd409abccb4eee16b4b825f782e0fbd1
Эмиль Алтынбаев <emilaltynbaev>
2020-09-20 18:47:48 +0300

    Part3

---
+++ part3/backend_hw/build/precache-manifest.7772caca4adbe3d8700bbf1aff4851ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512bfde107f160bde34d8e783b21e939",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e5cc78f16eeb6e7320c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5cc78f16eeb6e7320c",
+    "url": "/static/js/main.5a1b22ae.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
---
+++ part3/backend_hw/index.js
@@ -0,0 +1,109 @@
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+const cors = require('cors')
+var logger = require('morgan')
+logger.token('body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : null)
+const app = express()
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+app.use(logger(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+
+app.get('/info', (req, res) => {
+  Person.countDocuments({}, function (_err, count) {
+    res.send(`<p>Phonebook has info for ${count} people</p>
+            <p>${new Date()}</p>`)
+  })
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(person => {
+    response.json(person)
+  })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
